d1c7afe851c523cd5d88b7825cf37fca7d2b913c
Andrew Lu <awclu@ucdavis.edu>
2019-12-19 21:05:59 -0800

    done

--- bamazon.sql
+++ bamazon.sql
@@ -9,27 +9,26 @@ CREATE TABLE products(
     product_name VARCHAR(50) NOT NULL,
     department_name VARCHAR(30) NOT NULL,
     price int(10) NOT NULL,
-    stock_quantity int(10) NOT NULL
-    
+    stock int(10) NOT NULL
 );
 
-INSERT INTO products(product_name, department_name, price, stock_quantity)
+INSERT INTO products(product_name, department_name, price, stock)
 VALUE("Macbook Pro", "Computer and Electronics", 1200, 12);
-INSERT INTO products(product_name, department_name, price, stock_quantity)
+INSERT INTO products(product_name, department_name, price, stock)
 VALUE("Muscletech Protein Powder", "Fitness and Nutriton", 50, 100);
-INSERT INTO products(product_name, department_name, price, stock_quantity)
+INSERT INTO products(product_name, department_name, price, stock)
 VALUE("Metal Swing Arm Lamp", "Household Appliances", 25, 50);
-INSERT INTO products(product_name, department_name, price, stock_quantity)
+INSERT INTO products(product_name, department_name, price, stock)
 VALUE("Yakuza 2", "Video Games", 40, 10);
-INSERT INTO products(product_name, department_name, price, stock_quantity)
+INSERT INTO products(product_name, department_name, price, stock)
 VALUE("Death Stranding", "Video Games", 40, 10);
-INSERT INTO products(product_name, department_name, price, stock_quantity)
+INSERT INTO products(product_name, department_name, price, stock)
 VALUE("ON Preworkout Bluberry", "Fitness and Nutrition", 17, 20);
-INSERT INTO products(product_name, department_name, price, stock_quantity)
+INSERT INTO products(product_name, department_name, price, stock)
 VALUE("Asus GTX1060 Graphics Card", "Computer and Electronics", 200, 20);
-INSERT INTO products(product_name, department_name, price, stock_quantity)
+INSERT INTO products(product_name, department_name, price, stock)
 VALUE("Steelseries Mouse RIVAL 310", "Computer and Electronics", 40, 10);
-INSERT INTO products(product_name, department_name, price, stock_quantity)
+INSERT INTO products(product_name, department_name, price, stock)
 VALUE("48 Pack Soundproof Foam", "Household Appliances", 24, 10);
-INSERT INTO products(product_name, department_name, price, stock_quantity)
+INSERT INTO products(product_name, department_name, price, stock)
 VALUE("Pokemon Lost Thunder Booster Box", "Toys and Games", 100, 10);
--- customerView.js
+++ customerView.js
@@ -1,15 +1,5 @@
 var mysql = require("mysql");
-var axios = require("axios");
-
-var entryPrompt = [
-    {
-        type: "input", 
-        message: "What would you like to purchase today?",
-        name: "purchase"
-    }
-];
-
-
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -28,6 +18,90 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
+  readProducts();
   
   
 });
+
+
+function customerPrompt(productArr){
+
+    inquirer
+    .prompt([
+        {
+            type: "list", 
+            message: "What would you like to purchase today",
+            choices: productArr,
+            name: "purchase"
+        },
+        {
+            type: "input",
+            message: "How many units of this item would you like to purchase",
+            name: "quantity"
+        }
+    ])
+    .then(function(res){
+        
+        connection.query(
+            "UPDATE products SET stock = stock - "+res.quantity+" WHERE ?",
+            [
+                {
+                    product_name: res.purchase
+                }
+            ],
+            function(err,res){
+                console.log(res.affectedRows + " products updated!\n");
+                
+                
+        });
+        
+        console.log("Your Order of " + res.quantity + " " + res.purchase + " is complete");
+        continuePrompt();
+    })
+}
+
+function readProducts() {
+    console.log("Showing all products...\n");
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      var productArr = [];
+      // Log all results of the SELECT statement
+      console.log("------------------------------------------------------------------");
+      console.log("ID | Product Name | Product Department | Price | Stock");
+      for (i = 0; i < res.length; i++){
+          console.log("------------------------------------------------------------------");
+          console.log(res[i].id + " | " +res[i].product_name+ " | " + res[i].department_name + " | $" + res[i].price + " | " + res[i].stock + " |");
+          productArr.push(res[i].product_name);
+        //connection.end();
+          if (res[i].stock <= 0){
+              console.log ("The item " + res[i].product_name + " is out of stock");
+          }
+      }
+      customerPrompt(productArr);
+    });
+    
+}
+
+function continuePrompt(){
+
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "Would you like to continue shopping? (yes/no)",
+            name: "choices"
+        }
+    ])
+    .then(function(res){
+        
+        if (res.choices == "yes"){
+            readProducts();
+        }
+        else if (res.choices == "no"){
+            connection.end();
+        }
+        else{
+            console.log("Input yes or no");
+        }
+    })
+}
